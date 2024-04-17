2a0d8f3857c3425cf2808471330bb80fda387f7f
AlexOnDev <alejandrodepablozueco@gmail.com>
2024-04-17 17:03:47 +0200

    Inicio y variables

---
+++ 00hello.py
@@ -0,0 +1,21 @@
+# Hola mundo
+
+print("Hola mundo")
+print('Hola mundo')
+"""
+Esto es similar a /* 
+                   *
+                   *
+                   *
+                  */ 
+"""
+
+'''
+Tambien con comillas simples
+'''
+#Consultar tipo de dato
+print(type("Hola Python")) # Tipo str
+print(type(5)) # Tipo int
+print(type(1.4)) # Tipo float
+print(type(True)) # Tipo bool
+print(type(3 + 2j)) # Tipo complex
---
+++ 01variables.py
@@ -0,0 +1,36 @@
+# Variables
+my_string_variable = "My String Variable"
+print(my_string_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_bool_variable = False
+print(my_bool_variable)
+
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+# Concatenación de variables en un print
+print(my_string_variable,my_int_variable, my_bool_variable)
+print(type(print(my_string_variable,my_int_variable, my_bool_variable))) # print = NoneType
+print("Est es el valor de: ", my_bool_variable)
+
+# Algunas funciones del sistema
+print(len(my_string_variable))
+
+# Variables en una sola línea (esto deberia de ser ilegal)
+name, surname, alias, age= "Alejandro", "de Pablo", "AlexOnDev", 22
+print(name, surname, alias, age)
+
+# Inputs
+# name = input("¿Cual es tu nombre? ")
+# age = input("¿Cuantos años tienes? ")
+# print(name)
+# print(age)
+
+# ¿Forzamos el tipo?
+address: str = "Mi direccion"
+address = 32
+print(type(address))
