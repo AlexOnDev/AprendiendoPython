47cc8278c05e51ea6a47ff4f41cd9d05aa5d5344
AlexOnDev <alejandrodepablozueco@gmail.com>
2024-08-10 10:28:59 +0200

    08conditionals py acabado, iniciado 09loops.py

--- 08conditionals.py
+++ 08conditionals.py
@@ -1,1 +1,28 @@
 ### Conditionals ###
+
+my_condition = False
+
+if my_condition:
+    print("Positivo")
+
+my_condition = 1
+
+if my_condition > 10:
+    print("Mayor que 10")
+elif my_condition == 1:
+    print("Es igual a 1")
+else:
+    print("Es menor o igual que 10 y distino que 1")
+    print("Es menor o igual que 10 y distino que 1")
+print("Fuera del else")
+
+print("Fuera del if")
+
+my_string = "" # False
+
+if not my_string:
+    print("Cadena de texto esta vacia")
+if my_string == "":
+    print("Es igual la cadena")
+
+print(my_string)
