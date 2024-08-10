d0102e9df28f7ca1747ddc4e5aea80bdf6f85706
AlexOnDev <alejandrodepablozueco@gmail.com>
2024-08-11 00:57:03 +0200

    exceptions.py acabado, acabado modules.py, creado my_module.py

--- 13modules.py
+++ 13modules.py
@@ -1,1 +1,19 @@
 ### Modules ###
+
+import my_module
+
+my_module.sumValue(5,3,4)
+my_module.printValue("AAA")
+
+from my_module import sumValue, printValue
+
+sumValue(5,3, 4)
+printValue("holaaa")
+
+import math
+
+print(math.pi)
+print(math.trunc(math.pow(2, 8)))
+
+from math import pi as PI_VALUE
+print(PI_VALUE)
