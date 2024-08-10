baf84b1fedbf1ea2bb11f390fecdd4969e578a3a
AlexOnDev <alejandrodepablozueco@gmail.com>
2024-08-11 00:56:55 +0200

    exceptions.py acabado, acabado modules.py, creado my_module.py

--- 12exceptions.py
+++ 12exceptions.py
@@ -1,1 +1,36 @@
-### Exceptions ###
+### Exceptions Handling ###
+
+numberOne, numberTwo = 5, 1
+numberTwo  = "1"
+
+# try except
+try:
+    print(numberOne + numberTwo)
+except:
+    print("ERROR")
+
+# try except else finally
+try:
+    print(numberOne + numberTwo)
+except:
+    print("ERROR")
+else: # Opcional
+    # Se ejecuta si no hay excepción
+    print("La ejecucion continua correctamente")
+finally: # Opcional
+    # Se ejecuta siempre
+    print("La ejecución continúa")
+
+ # Excepciones por tipo
+try:
+    print(numberOne + numberTwo)
+except ValueError:
+    print("ERROR")
+except TypeError:
+    print("ERROR")
+
+ # Captura de la información de la excepción
+try:
+    print(numberOne + numberTwo)
+except Exception as error:
+    print(error)
