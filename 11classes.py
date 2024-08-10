7fd12d292011530364e181162cb64e254a82d2d2
AlexOnDev <alejandrodepablozueco@gmail.com>
2024-08-10 12:41:48 +0200

    clases.py finalizado, exceptions inicio

--- 11classes.py
+++ 11classes.py
@@ -1,1 +1,34 @@
 ### Classes ###
+
+class MyEmptyPerson:
+    pass # Para poner vacia la clase sin error
+
+print(MyEmptyPerson)
+print(MyEmptyPerson())
+
+class Person:
+    def __init__(self, name, surname, alias = "Sin alias"): # Método constructor
+        self.full_name = f"{name} {surname} ({alias})" # Propiedad pública
+        self.__name = name # Propiedad privada
+        self.__surname = surname
+
+    def get_name(self): # Getter enmascarado en una función
+        return self.__name
+    def walk(self):
+        print(f"{self.full_name} está caminando")
+
+my_person = Person("Alejandro", "de Pablo")
+print(my_person.full_name)
+my_person.walk()
+
+my_other_person = Person("Alejandro", "de Pablo", "Alejandro Corp")
+print(my_other_person.full_name)
+my_other_person.walk()
+
+my_other_person.full_name = "Michael \"The Goat\" Jordan"
+print(my_other_person.full_name)
+
+my_other_person.full_name = 888
+print(my_other_person.full_name)
+
+print(my_other_person.get_name())
