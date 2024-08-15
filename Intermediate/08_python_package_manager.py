715aeb9398eee5c7254123d9ed8d9b80dbeb871f
AlexOnDev <alejandrodepablozueco@gmail.com>
2024-08-15 11:23:03 +0200

    08_python_package_manager.py junto a paquete

---
+++ Intermediate/08_python_package_manager.py
@@ -0,0 +1,35 @@
+### Python Package Manager ###
+
+# PIP https://pypi.org
+
+import numpy
+
+import mypackage.arithmetics # pip install numpy
+print(numpy.version.version)
+
+numpy_array = numpy.array([35, 24, 65, 30, 30, 16])
+print(type(numpy_array))
+
+print(numpy_array * 2)
+
+import pandas # pip install pandas
+
+# pip list
+# pip unistall pandas
+# pip show numpy
+
+import requests # pip install requests
+
+response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=151")
+print(response)
+print(response.status_code)
+print(response.json())
+
+# Arithmetics Package
+from mypackage import arithmetics
+
+print(arithmetics.sum_two_values(1,4))
+
+from myotherpackage import my_other_arithmetics
+
+print(my_other_arithmetics.sum_two_values(1,4))
---
+++ Intermediate/mypackage/arithmetics.py
@@ -0,0 +1,4 @@
+### Arithmetics ###
+
+def sum_two_values(a : int, b): # : int es para aconsejar al programador
+    return (a + b)
