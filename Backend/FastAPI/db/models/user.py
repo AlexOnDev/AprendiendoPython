195624a9a237e4ea4ccdba2b672049ec51fc195d
AlexOnDev <alejandrodepablozueco@gmail.com>
2024-08-17 12:40:41 +0200

    Operacion post (create) lista y get usuarios listo

--- Backend/FastAPI/db/models/user.py
+++ Backend/FastAPI/db/models/user.py
@@ -4,6 +4,6 @@ from typing import Optional
 # Entidad user
 class User(BaseModel):
     # id: str | None  # String porque mongo los crea string para crear id unicos mas facil
-    id: Optional [str] = None # Da null si no error
+    id: Optional [str] = None # Error 422
     username: str
     email: str
--- Backend/FastAPI/routers/users_db.py
+++ Backend/FastAPI/routers/users_db.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, HTTPException, status
 import logging
 from db.models.user import User
-from db.schemas.user import user_schema
+from db.schemas.user import user_schema, users_schema
 from db.client import db_client
+from bson import ObjectId 
 
 router = APIRouter(prefix="/userdb", 
                    tags=["userdb"], 
@@ -10,35 +11,38 @@ router = APIRouter(prefix="/userdb",
 
 users_list = []
 
+# python -m uvicorn main:app --log-level debug
 logger = logging.getLogger('uvicorn.error')
 logger.setLevel(logging.DEBUG)
 
 
-@router.get("/")
+@router.get("/", response_model=list[User]) # Valida FastApi y ademas lo mete en la docu
 async def users():
-    return users_list
+    return users_schema(db_client.local.users.find())
 
 # Path
 @router.get("/{id}")
-async def user(id: int): 
-    return search_user(id)
+async def user(id: str): 
+    return search_user("_id", ObjectId(id)) #Oid como el que tiene mongodb en cada elemento
     
 # Query
 @router.get("/") 
-async def user(id: int):
-    return search_user(id)
+async def user(id: str):
+    return search_user("_id", ObjectId(id))
 
 # Post  
 @router.post("/", response_model=User, status_code=status.HTTP_201_CREATED)
 async def user(user: User):
-    # if type(search_user(user.id)) == User:
-    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="El usuario ya existe")
+    if type(search_user("email", user.email)) == User:
+         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="El usuario ya existe")
+    
     user_dict = dict(user)
+    # logger.debug(user_dict)
     del user_dict["id"] # Asi mongoDB autogenera el id
 
     id = db_client.local.users.insert_one(user_dict).inserted_id # MongoDB funciona con JSON
-    logger.debug(id)
     new_user = user_schema(db_client.local.users.find_one({"_id":id})) # _id  # (formato JSON)
+    # logger.debug(new_user)
     
     
     return User(**new_user)
@@ -74,10 +78,9 @@ async def user(id: int):
          return {"error":"No se ha eliminado el usuario"}
 
 
-def search_user(id: int):
-    users = filter(lambda user: user.id ==id, users_list)
+def search_user(field: str, key):
     try:
-        return list(users)[0]
+        user = user_schema(db_client.local.users.find_one({field: key}))
+        return User(**user)
     except:
         return {"error":"No se ha encontrado el usuario"}
-
