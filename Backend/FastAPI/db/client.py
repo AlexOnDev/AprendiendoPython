57548e4083ee268e57bba23e689da103b34bffa3
AlexOnDev <alejandrodepablozueco@gmail.com>
2024-08-18 08:31:51 +0200

    bd remota conexion client.py

--- Backend/FastAPI/db/client.py
+++ Backend/FastAPI/db/client.py
@@ -1,4 +1,7 @@
 from pymongo import MongoClient
 
-db_client = MongoClient().local
+# BD Local
+# db_client = MongoClient().local
 
+# BD Remota
+db_client = MongoClient("mongodb+srv://test:test@cluster0.2ooeo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").test
